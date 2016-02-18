d44d0f1268fee91fc91ecbf0add5d9fc7b46b56c
Gabriel <pabombs@gmail.com>
2016-02-18 15:59:44 -0500

    returned to parsing lines of files to make code more sturdy

--- src/Assignment2/PublicationListingProcess1.java
+++ src/Assignment2/PublicationListingProcess1.java
@@ -9,8 +9,10 @@ import java.io.*;
 public class PublicationListingProcess1 {
     static Scanner userInput = new Scanner(System.in);
     public static Publication[] publicationArray;
-    private static final String READPATH = "src/Assignment2/PublicationData_Input.txt";
+    private static final String READFILENAME = "PublicationData_Input.txt";
+    private static final String READPATH = "src/Assignment2/";
     
+    private static File fileToWrite;
     private static Scanner reader;
     private static BufferedWriter writer;
     
@@ -24,7 +26,6 @@ public class PublicationListingProcess1 {
     }
     
     public static void main(String[] args) {
-        File fileToWrite;
         System.out.print("Specify the name of the output file > ");
         while(true) {
             fileToWrite = new File("src/Assignment2/" + userInput.next() + ".txt");
@@ -37,23 +38,23 @@ public class PublicationListingProcess1 {
             }
         }
         try {
-            reader = new Scanner(new FileReader(READPATH));
-            writer = new BufferedWriter(new FileWriter(fileToWrite));
-            publicationArray = new Publication[getNumberOfPublications()];
+            reader = new Scanner(new FileReader(READPATH + READFILENAME));
+            writer = new BufferedWriter(new FileWriter(fileToWrite, true));
+            publicationArray = arrayMaker(READFILENAME);
             if(publicationArray.length <= 1) {
                 //handle small file
                 System.out.println("File too small to have duplacates");
                 //
-                reader.close();
-                writer.close();
+//                reader.close();
+//                writer.close();
             }
             else {
-                correctListOfItems(reader, writer);
-                reader.close();
-                writer.close();
+                //correctListOfItems(reader, writer);
+                printFileItems(fileToWrite);
+//                reader.close();
             }
         } catch (FileNotFoundException ex) {
-            System.out.println("The file " + READPATH + " is missing, please place it in \"src/Assignment2/\" and restart the program.");
+            System.out.println("The file " + READFILENAME + " is missing, please place it in \"src/Assignment2/\" and restart the program." + Arrays.toString(ex.getStackTrace()));
             System.exit(0);
         } catch (IOException ex) {
             //handle exception
@@ -61,9 +62,6 @@ public class PublicationListingProcess1 {
     }
     
     public static void correctListOfItems(Scanner input, BufferedWriter output) throws IOException {
-        for(int i = 0; i < publicationArray.length; i++) { 
-            publicationArray[i] = new Publication(reader.nextLong(), reader.next(), reader.nextInt(), reader.next(), reader.nextDouble(), reader.nextInt());
-        }
         while(true) {
             try {
                 for(int i = 0 ; i < publicationArray.length ; i++) {
@@ -82,12 +80,22 @@ public class PublicationListingProcess1 {
         }
     }
     
-    public void printFileItems(File outputFile) throws FileNotFoundException {
-        
+    public static void printFileItems(File outputFile) throws FileNotFoundException {
+        System.out.println("Original file :\n");
+        for(int i = 0 ; i < publicationArray.length ; i++) {
+            System.out.print((i + 1) + ". ");
+            for(int j = 0 ; j < 6 ; j++) {
+                if(reader.hasNext() || true) {
+                    System.out.print(reader.next() + "  ");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("Corrected file");
     }
     
-    public static int getNumberOfPublications() throws FileNotFoundException, IOException {
-        BufferedReader input = new BufferedReader(new FileReader(READPATH));
+    public static int getNumberOfPublications(String path) throws FileNotFoundException, IOException {
+        BufferedReader input = new BufferedReader(new FileReader(path));
         int nbPublications = 0;
         String line;
         while(true) {
@@ -99,8 +107,25 @@ public class PublicationListingProcess1 {
                 nbPublications++;
             }
         }
-        input.close();
         return nbPublications;
+    }
+    
+    public static Publication[] arrayMaker(String name) throws FileNotFoundException, IOException{
+        Scanner input = new Scanner(new FileReader(READPATH + name));
+        Publication[] array = new Publication[getNumberOfPublications(READPATH + name)];
+        String[] fileContent;
+        for(int i = 0; i < array.length; i++) { 
+            fileContent = split(input.nextLine(), "whitespace");
+            try {    
+                array[i] = new Publication(Long.parseLong(fileContent[0]), fileContent[1], 
+                                            Integer.parseInt(fileContent[2]), fileContent[3], 
+                                            Double.parseDouble(fileContent[4]), Integer.parseInt(fileContent[5]));
+            } catch (NumberFormatException ex) {
+                publicationArray[i] = new Publication();
+                System.out.println("The format of publication no." + (i + 1) + " is incorrect and has been skipped");
+            }
+        }
+        return array;
     }
     
     static String[] split(String entry, String seperator) {

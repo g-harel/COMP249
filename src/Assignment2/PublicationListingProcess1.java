a3eb3d33ae87f3582e280481b4143eeee49a3427
Gabriel <pabombs@gmail.com>
2016-02-23 21:20:12 -0500

    added basic comments

--- src/Assignment2/PublicationListingProcess1.java
+++ src/Assignment2/PublicationListingProcess1.java
@@ -22,9 +22,10 @@ public class PublicationListingProcess1 {
     }
     
     public static void main(String[] args) {
-        
+        //asking user for output file name
         File fileToWrite;
         System.out.print("Specify the name of the output file > ");
+        //making sure the file doesn't already exist
         while(true) {
             fileToWrite = new File(READPATH + userInput.next() + ".txt");
             userInput.nextLine();
@@ -35,33 +36,37 @@ public class PublicationListingProcess1 {
                 break;
             }        
         }
+        //reading the file, changing duplicate codes and printing both new and old files using some helper methods
         try {
             fileToWrite.createNewFile();
-            if(!(new File(READPATH + READFILENAME)).exists()) {
-                throw new FileNotFoundException(READFILENAME);
-            }
+            //case when the number of lines in the file is too small
             if(getLines(READPATH + READFILENAME) <= 1) {
-                //handle small file
-                System.out.println("File too small to have duplacates");
-                //
+                System.out.println("File too small to have duplicates");
             }
+            //otherwise
             else {
+                //method called to verify that there are no duplicates and asks the user to change them if there are any
+                //also creates a new file with the corrected set of publications
                 correctListOfItems(READPATH + READFILENAME, READPATH + fileToWrite.getName());
+                //printing out the old and new files
                 System.out.println("\nOld file :");
                 printFileItems(READPATH + READFILENAME);
                 System.out.println("\nNew file :");
                 printFileItems(READPATH + fileToWrite.getName());
             }
         } catch (FileNotFoundException ex) {
-            System.out.println("The file " + ex.getMessage() + " is missing, please place it in \"" + READPATH + "\" and restart the program." + Arrays.toString(ex.getStackTrace()));
+            //error if one of the files is not found
+            System.out.println("The file is missing, please place it in \"" + READPATH + "\" and restart the program." + Arrays.toString(ex.getStackTrace()));
             System.exit(0);
         } catch (IOException ex) {
-            //handle exception
+            //error for any other IO related exception
+            System.out.println("Something went wrong with the file!");
+            System.out.println(ex.getMessage());
         }
     }
     
     /**
-     * Checks that there are no duplicated publication codes in an array of Publication objects
+     * checks that there are no duplicated publication codes in an array of Publication objects
      * 
      * @param inputName
      * @param outputName
@@ -69,7 +74,9 @@ public class PublicationListingProcess1 {
      */
     public static void correctListOfItems(String inputName, String outputName) throws IOException {
         System.out.println();
+        //creating an array out of the file to be read
         Publication[] publicationArray = arrayMaker(inputName);
+        //looking for repeated publication codes
         while(true) {
             try {
                 for(int i = 0 ; i < publicationArray.length ; i++) {
@@ -81,11 +88,13 @@ public class PublicationListingProcess1 {
                 }
                 break;
             } catch (CopyCodeException ex) {
+                    //error if two codes are the same
                     System.out.print(", enter the new code > ");
                     publicationArray[ex.getToChange()].setPublicationCode(userInput.next());
                     userInput.nextLine();
             }
         }
+        //outputting to new file
         try(BufferedWriter output = new BufferedWriter(new FileWriter(outputName, true))) {
             for (Publication i : publicationArray) {
                 output.write(i.toString() + "\n");
@@ -94,17 +103,18 @@ public class PublicationListingProcess1 {
     }
     
     /**
-     * Prints the contents of a file to the console
+     * prints the contents of a file to the console
      * 
      * @param outputFile
      * @throws FileNotFoundException, IOException
      */
     private static void printFileItems(String inputName) throws FileNotFoundException, IOException {
+        //calls the method of the same name in the other listing process class
         PublicationListingProcess2.printFileItems(inputName);
     }
     
     /**
-     * Returns the number of non-empty lines found in the inputted filename in the READPATH directory
+     * returns the number of non-empty lines found in the inputted filename in the READPATH directory
      * 
      * @param name
      * @return
@@ -117,6 +127,7 @@ public class PublicationListingProcess1 {
             String line;
             while(true) {
                 line = reader.readLine();
+                //increment if line exists and is not empty
                 if(line != null && !line.equals("")) {
                     nbPublications++;
                     continue;
@@ -128,7 +139,7 @@ public class PublicationListingProcess1 {
     }
     
     /**
-     * Makes an array of Publications from the inputted filename in the READPATH directory
+     * makes an array of Publications from the inputted filename in the READPATH directory
      * 
      * @param inputName
      * @return
@@ -137,14 +148,17 @@ public class PublicationListingProcess1 {
      */
     public static Publication[] arrayMaker(String inputName) throws FileNotFoundException, IOException{
         try(BufferedReader reader = new BufferedReader(new FileReader(inputName))) {
+            //creating the array of the right lenght
             Publication[] array = new Publication[getLines(inputName)];
             String[] fileContent;
+            //reading the file line by line and parsing each entry
             for(int i = 0; i < array.length; i++) {
                 fileContent = split(reader.readLine()); 
-                try {    
+                try {
                     array[i] = new Publication(Long.parseLong(fileContent[0]), fileContent[1], Integer.parseInt(fileContent[2]), fileContent[3], 
                                                 Double.parseDouble(fileContent[4]), Integer.parseInt(fileContent[5]));
                 } catch (NumberFormatException ex) {
+                    //error if one of the entries is of the wrong format
                     array[i] = new Publication();
                     System.out.println("The format of publication no." + (i + 1) + " is incorrect and has been skipped");
                 }
@@ -155,14 +169,15 @@ public class PublicationListingProcess1 {
     }
     
     /**
-     * Splits inputted strings every whitespace and returns an array of the substrings
+     * splits inputted strings every whitespace and returns an array of the substrings
      * 
      * @param toSplit
      * @return 
      */
     public static String[] split(String toSplit) {
+        //making all whitespace characters simply spaces
         String allSpace = toSplit.trim().replace('\t', ' ').replace('\n', ' ');
-        //not sure if necessary
+        //removing duplicate spaces
         String temp = "";
         for(int i = 0 ; i < allSpace.length() ; i++) {
             if(!(i > 0 && allSpace.charAt(i) == ' ' && allSpace.charAt(i-1) == ' ')) {

08dc71b7da439cb3a4ccc5bf3abf8fabbbf7521b
Gabriel <pabombs@gmail.com>
2016-02-23 20:22:01 -0500

    added basic comments

--- src/Assignment2/CopyCodeException.java
+++ src/Assignment2/CopyCodeException.java
@@ -6,14 +6,17 @@ package Assignment2;
  */
 public class CopyCodeException extends Exception{
     
+    //variable that stores the id of the publication causing the error
     private int toChange;
     
+    //custom contructor to initialize the to Change variable
     CopyCodeException(String message, int toChange) {
         super(message);
         System.out.print(message);
         this.toChange = toChange;
     }
     
+    //returns toChange variable
     public int getToChange(){
         return toChange;
     }

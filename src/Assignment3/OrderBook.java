4d1654f792c18253d790b80641c66082faadaf7b
Gabriel <pabombs@gmail.com>
2016-03-18 22:18:52 -0400

    OrderBook no longer extends LinkedList (horrible idea ot begin with), adding process not fully working

--- src/Assignment3/OrderBook.java
+++ src/Assignment3/OrderBook.java
@@ -3,51 +3,77 @@ package Assignment3;
 import java.util.LinkedList;
 
 /**
- *
- * @author Gabriel
+ * Created by Gabriel on 2016-03-18.
  */
-public class OrderBook extends LinkedList{
-    
-    private int midpoint/*number of negative*/;
-    
-    OrderBook () {
-        super();
-        setMidpoint(0);
+public class OrderBook {
+
+    private LinkedList<Order> orders;
+    private int bestBid;
+    private int bestOffer;
+
+    OrderBook() {
+        orders = new LinkedList<>();
+        bestBid = 0;
+        bestOffer = 0;
     }
 
-    //adds a new order into the list while keeping the sorted nature
-    public void newOrder(Order ord) {
-        for(int i = 0; i < this.size(); i++) {
-            if(ord.getPrice() >= ((Order)this.get(i)).getPrice()) {
-                this.add(i+1, ord);
-                setMidpoint(-1);
+    public void add(Order ord) {
+        System.out.println("adding  > " + ord.toString());
+        if(orders.size() == 0) {
+            orders.add(ord);
+        }
+        for(int i = 0; i < orders.size(); i++) {
+            if(ord.getPrice() >= (orders.get(i)).getPrice()) {
+                orders.add(i+1, ord);
+                setBest();
                 break;
             }
         }
     }
 
-    //sets the midpoint (first positive value ~> first offer) to the parameter, or finds it if the parameter == -1
-    public void setMidpoint(int pos) {
-        if(pos == -1) {
-            //finds first positive value
-            for(int i = 0; i < this.size(); i++) {
-                if(((Order)this.get(i)).getPrice() > 0) {
-                    this.midpoint = i;
-                    break;
+    public void matchingEngine() {
+        System.out.println("starting matching process");
+        for(int i = this.bestOffer; i < orders.size(); i++) {
+            System.out.print("o");
+            for(int j = 0; i < this.bestBid; i++) {
+                System.out.print("b" + i + " " + j);
+                if(orders.get(i).getPrice() <= orders.get(j).getPrice()) {
+                    System.out.println("match found");
+                    smash(j, i);
+                    matchingEngine();
+                    return;
                 }
             }
-        } else {
-            this.midpoint = pos;
         }
     }
-    
-    public int getMidpoint() {
-        return this.midpoint;
+
+    public void smash(int bid, int offer) {
+        System.out.println("match processing");
+        //if offer >= bid smash number of bids from offers and delete bid object
+        if(orders.get(offer).remove(orders.get(bid).getVolume())) {
+            //if offer is left at 0 after the removal, delete the offer object first (it is after the offer's index and therefore deleting won't change it)
+            if(orders.get(offer).getVolume() == 0) {
+                orders.remove(offer);
+            }
+            orders.remove(bid);
+            //otherwise smash number of offers from bids and delete offer object
+        } else {
+            orders.get(bid).remove(orders.get(offer).getVolume());
+            orders.remove(offer);
+        }
     }
 
-    //will remove element and set the midpoint to the right place
-    public void removeOrder(int pos) {
-        super.remove(pos);
-        setMidpoint(-1);
+    public void setBest() {
+        for(int i = 0; i < orders.size(); i++) {
+            if(orders.get(i).getPrice() > 0) {
+                this.bestOffer = i;
+                this.bestBid = i - 1;
+                break;
+            }
+        }
+    }
+
+    public String toString() {
+        return orders.toString();
     }
 }
--- src/Assignment3/StockMarket.java
+++ src/Assignment3/StockMarket.java
@@ -5,43 +5,18 @@ package Assignment3;
  * @author Gabriel
  */
 public class StockMarket {
-    
-    OrderBook orders = new OrderBook();
+
+    static OrderBook book = new OrderBook();
     
     public static void main(String[] args) {
-        System.out.println("sdf");
-    }
-    
-    public void matchingEngine() {
-        //iterate through offers
-        for(int i = orders.getMidpoint(); i < orders.size(); i++) {
-            //iterate through bids
-            for(int j = 0; i < orders.getMidpoint(); i++) {
-                //if an offer is <= to a bid, match them together and process it
-                if(((Order)orders.get(i)).getPrice() <= -((Order)orders.get(j)).getPrice()) {
-                    processMatch(i, j);
-                    //if a match is made, the OrderBook object has changed and this method needs to be called again
-                    matchingEngine();
-                    //to avoid going through the rest of the array uselessly
-                    return;
-                }
-            }
-        }
-        //does nothing if no matches are found
-    }
-    
-    public void processMatch(int offer, int bid) {
-        //if offer >= bid remove number of bids from offers and delete bid object
-        if(((Order)orders.get(offer)).remove(((Order)orders.get(bid)).getVolume())) {
-            //if offer is left at 0 after the removal, delete the offer object aswell
-            if(((Order)orders.get(offer)).getVolume() == 0) {
-                orders.removeOrder(offer);
-            }
-            orders.removeOrder(bid);
-        //otherwise remove number of offers from bids and delete offer object
-        } else {
-            ((Order)orders.get(bid)).remove(((Order)orders.get(offer)).getVolume());
-            orders.removeOrder(offer);
-        }
+        System.out.println("starting");
+        book.add(new OfferOrder(123, 20.2, 33));
+        book.add(new OfferOrder(125, 21.0, 21));
+        book.add(new BidOrder(122, 21.0, 21));
+        book.add(new BidOrder(132, 10.0, 21));
+        System.out.println(book.toString());
+        book.matchingEngine();
+        System.out.println(book.toString());
+        System.out.println("done");
     }
 }
